6af1c944b0eaa5fde4183cd94f00ee53f3c1e31a
KatyaFleur <katyafleur@yandex.ru>
2022-11-18 17:12:44 +0300

    5. Подчищен catalog.section.list, добавлена верстка основных разделов с товарами в слайдере, добавлена логика вывода блока "распродажа" и "просмотренные".

--- local/templates/bike_shop_inner/components/bitrix/catalog.section.list/bootstrap_v4/template.php
+++ local/templates/bike_shop_inner/components/bitrix/catalog.section.list/bootstrap_v4/template.php
@@ -37,26 +37,6 @@ $arViewStyles = array(
 );
 $arCurView = $arViewStyles[$arParams['VIEW_MODE']];
 
-switch ($arParams['LIST_COLUMNS_COUNT']) {
-    case "1":
-        $listColumsClass = "col-12";
-        break;
-    case "2":
-        $listColumsClass = "col-6";
-        break;
-    case "3":
-        $listColumsClass = "col-sm-4 col-6";
-        break;
-    case "4":
-        $listColumsClass = "col-md-3 col-sm-4 col-6";
-        break;
-    case "6":
-        $listColumsClass = "col-lg-2 col-md-3 col-sm-4 col-6";
-        break;
-    case "12":
-        $listColumsClass = "col-lg-1 col-md-3 col-sm-4 col-6";
-        break;
-}
 
 $strSectionEdit = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "SECTION_EDIT");
 $strSectionDelete = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "SECTION_DELETE");
@@ -78,220 +58,268 @@ $arSectionDeleteParams = array("CONFIRM" => GetMessage('CT_BCSL_ELEMENT_DELETE_C
     </h2><?
 }
 
-if (0 < $arResult["SECTIONS_COUNT"])
-{
-?>
-<?
+if (0 < $arResult["SECTIONS_COUNT"]) {
+    ?>
+    <?
 
-switch ($arParams['VIEW_MODE'])
-{
-case 'LINE':
-    foreach ($arResult['SECTIONS'] as &$arSection) {
-        $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
-        $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
-
-        if (false === $arSection['PICTURE'])
-            $arSection['PICTURE'] = array(
-                'SRC' => $arCurView['EMPTY_IMG'],
-                'ALT' => (
-                '' != $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_ALT"]
-                    ? $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_ALT"]
-                    : $arSection["NAME"]
-                ),
-                'TITLE' => (
-                '' != $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_TITLE"]
-                    ? $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_TITLE"]
-                    : $arSection["NAME"]
-                )
-            );
-        ?>
-    <li id="<? echo $this->GetEditAreaId($arSection['ID']); ?>" class="catalog-section-list-item">
-        <div class="catalog-section-list-line-img-container">
-            <a
-                    href="<? echo $arSection['SECTION_PAGE_URL']; ?>"
-                    class="catalog-section-list-item-img"
-                    style="background-image:url('<? echo $arSection['PICTURE']['SRC']; ?>');"
-                    title="<? echo $arSection['PICTURE']['TITLE']; ?>"
-            ></a>
-        </div>
-        <div class="catalog-section-list-item-inner">
-            <h3 class="catalog-section-list-item-title">
-                <a class="catalog-section-list-item-link" href="<? echo $arSection['SECTION_PAGE_URL']; ?>">
-                    <? echo $arSection['NAME']; ?>
-                </a>
-                <? if ($arParams["COUNT_ELEMENTS"] && $arSection['ELEMENT_CNT'] !== null) {
-                    ?>
-                    <span class="catalog-section-list-item-counter">(<? echo $arSection['ELEMENT_CNT']; ?>)</span>
-                    <?
+    switch ($arParams['VIEW_MODE']) {
+        case 'TEXT':
+            $i = 0;
+            foreach ($arResult['SECTIONS'] as &$arSection) {
+                $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
+                $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
+                $i++;
+                ?>
+                <? if ($i == 3) {
+                    echo 'блок';
+                    // Просмотренные товары
+                    $APPLICATION->IncludeComponent(
+	"bitrix:catalog.viewed.products", 
+	"viewed_products", 
+	array(
+		"SHOW_FROM_SECTION" => "N",
+		"IBLOCK_ID" => "4",
+		"SECTION_ID" => "",
+		"SECTION_CODE" => "",
+		"SECTION_ELEMENT_ID" => "",
+		"SECTION_ELEMENT_CODE" => "",
+		"HIDE_NOT_AVAILABLE" => "N",
+		"SHOW_DISCOUNT_PERCENT" => "Y",
+		"PRODUCT_SUBSCRIPTION" => "N",
+		"SHOW_NAME" => "Y",
+		"SHOW_IMAGE" => "Y",
+		"MESS_BTN_BUY" => "Купить",
+		"MESS_BTN_DETAIL" => "Подробнее",
+		"MESS_BTN_SUBSCRIBE" => "Подписаться",
+		"PAGE_ELEMENT_COUNT" => "30",
+		"DETAIL_URL" => "",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_NOTES" => "",
+		"CACHE_GROUPS" => "Y",
+		"SHOW_OLD_PRICE" => "N",
+		"PRICE_CODE" => array(
+			0 => "BASE",
+		),
+		"SHOW_PRICE_COUNT" => "1",
+		"PRICE_VAT_INCLUDE" => "Y",
+		"CONVERT_CURRENCY" => "N",
+		"BASKET_URL" => "/personal/basket.php",
+		"ACTION_VARIABLE" => "action",
+		"PRODUCT_ID_VARIABLE" => "id",
+		"PRODUCT_QUANTITY_VARIABLE" => "quantity",
+		"ADD_PROPERTIES_TO_BASKET" => "Y",
+		"PRODUCT_PROPS_VARIABLE" => "prop",
+		"PARTIAL_PRODUCT_PROPERTIES" => "N",
+		"USE_PRODUCT_QUANTITY" => "N",
+		"SHOW_PRODUCTS_2" => "N",
+		"PROPERTY_CODE_2" => "",
+		"CART_PROPERTIES_2" => "",
+		"ADDITIONAL_PICT_PROP_2" => "-",
+		"LABEL_PROP_2" => "-",
+		"PROPERTY_CODE_3" => "",
+		"CART_PROPERTIES_3" => "",
+		"ADDITIONAL_PICT_PROP_3" => "-",
+		"OFFER_TREE_PROPS_3" => array(
+			0 => "-",
+		),
+		"COMPONENT_TEMPLATE" => "viewed_products",
+		"IBLOCK_TYPE" => "catalog",
+		"DEPTH" => "2",
+		"LINE_ELEMENT_COUNT" => "3",
+		"TEMPLATE_THEME" => "blue",
+		"SHOW_PRODUCTS_4" => "Y",
+		"ADDITIONAL_PICT_PROP_4" => "MORE_PHOTO",
+		"LABEL_PROP_4" => "-",
+		"ADDITIONAL_PICT_PROP_5" => ""
+	),
+	false
+);
+                } else if ($i == 5) {
+                    echo 'блок предпоследний';
+                    // Элементы раздела
+                    $APPLICATION->IncludeComponent(
+	"bitrix:catalog.section", 
+	"bootstrap_v4", 
+	array(
+		"IBLOCK_TYPE" => "catalog",
+		"IBLOCK_ID" => "4",
+		"SECTION_ID" => "21",
+		"SECTION_CODE" => "",
+		"SECTION_USER_FIELDS" => array(
+			0 => "",
+			1 => "",
+		),
+		"ELEMENT_SORT_FIELD" => "sort",
+		"ELEMENT_SORT_ORDER" => "asc",
+		"ELEMENT_SORT_FIELD2" => "id",
+		"ELEMENT_SORT_ORDER2" => "desc",
+		"FILTER_NAME" => "arrFilter",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"SHOW_ALL_WO_SECTION" => "N",
+		"HIDE_NOT_AVAILABLE" => "N",
+		"PAGE_ELEMENT_COUNT" => "4",
+		"LINE_ELEMENT_COUNT" => "3",
+		"PROPERTY_CODE" => "",
+		"OFFERS_LIMIT" => "5",
+		"SECTION_URL" => "",
+		"DETAIL_URL" => "",
+		"SECTION_ID_VARIABLE" => "SECTION_ID",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_NOTES" => "",
+		"CACHE_GROUPS" => "Y",
+		"SET_TITLE" => "Y",
+		"SET_BROWSER_TITLE" => "Y",
+		"BROWSER_TITLE" => "-",
+		"SET_META_KEYWORDS" => "Y",
+		"META_KEYWORDS" => "-",
+		"SET_META_DESCRIPTION" => "Y",
+		"META_DESCRIPTION" => "-",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"SET_STATUS_404" => "N",
+		"CACHE_FILTER" => "N",
+		"ACTION_VARIABLE" => "action",
+		"PRODUCT_ID_VARIABLE" => "id",
+		"PRICE_CODE" => array(
+			0 => "BASE",
+		),
+		"USE_PRICE_COUNT" => "N",
+		"SHOW_PRICE_COUNT" => "1",
+		"PRICE_VAT_INCLUDE" => "Y",
+		"CONVERT_CURRENCY" => "N",
+		"BASKET_URL" => "/personal/basket.php",
+		"USE_PRODUCT_QUANTITY" => "N",
+		"PRODUCT_QUANTITY_VARIABLE" => "quantity",
+		"ADD_PROPERTIES_TO_BASKET" => "Y",
+		"PRODUCT_PROPS_VARIABLE" => "prop",
+		"PARTIAL_PRODUCT_PROPERTIES" => "N",
+		"PRODUCT_PROPERTIES" => "",
+		"DISPLAY_COMPARE" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Товары",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"COMPONENT_TEMPLATE" => "bootstrap_v4",
+		"CUSTOM_FILTER" => "{\"CLASS_ID\":\"CondGroup\",\"DATA\":{\"All\":\"AND\",\"True\":\"True\"},\"CHILDREN\":[]}",
+		"HIDE_NOT_AVAILABLE_OFFERS" => "N",
+		"OFFERS_SORT_FIELD" => "sort",
+		"OFFERS_SORT_ORDER" => "asc",
+		"OFFERS_SORT_FIELD2" => "id",
+		"OFFERS_SORT_ORDER2" => "desc",
+		"OFFERS_FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"BACKGROUND_IMAGE" => "-",
+		"TEMPLATE_THEME" => "blue",
+		"PRODUCT_ROW_VARIANTS" => "[{'VARIANT':'3','BIG_DATA':false}]",
+		"ENLARGE_PRODUCT" => "STRICT",
+		"PRODUCT_BLOCKS_ORDER" => "price,props,sku,quantityLimit,quantity,buttons",
+		"SHOW_SLIDER" => "Y",
+		"PRODUCT_DISPLAY_MODE" => "N",
+		"ADD_PICT_PROP" => "MORE_PHOTO",
+		"LABEL_PROP" => array(
+		),
+		"PRODUCT_SUBSCRIPTION" => "Y",
+		"SHOW_DISCOUNT_PERCENT" => "N",
+		"SHOW_OLD_PRICE" => "N",
+		"SHOW_MAX_QUANTITY" => "N",
+		"SHOW_CLOSE_POPUP" => "N",
+		"MESS_BTN_BUY" => "Купить",
+		"MESS_BTN_ADD_TO_BASKET" => "В корзину",
+		"MESS_BTN_SUBSCRIBE" => "Подписаться",
+		"MESS_BTN_DETAIL" => "Подробнее",
+		"MESS_NOT_AVAILABLE" => "Нет в наличии",
+		"RCM_TYPE" => "personal",
+		"RCM_PROD_ID" => $_REQUEST["PRODUCT_ID"],
+		"SHOW_FROM_SECTION" => "N",
+		"SEF_MODE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"USE_MAIN_ELEMENT_SECTION" => "N",
+		"ADD_TO_BASKET_ACTION" => "ADD",
+		"USE_ENHANCED_ECOMMERCE" => "N",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"LAZY_LOAD" => "N",
+		"MESS_BTN_LAZY_LOAD" => "Показать ещё",
+		"LOAD_ON_SCROLL" => "N",
+		"SHOW_404" => "N",
+		"MESSAGE_404" => "",
+		"COMPATIBLE_MODE" => "Y",
+		"DISABLE_INIT_JS_IN_COMPONENT" => "N",
+		"SLIDER_INTERVAL" => "3000",
+		"SLIDER_PROGRESS" => "N"
+	),
+	false
+);
                 }
                 ?>
-            </h3>
-            <? if ('' != $arSection['DESCRIPTION']) {
-                ?>
-                <p class="catalog-section-list-item-description"><? echo $arSection['DESCRIPTION']; ?></p>
-                <?
-            }
-            ?>
-        </div>
-
-        </li><?
-    }
-    unset($arSection);
-    break;
-
-case 'TEXT':
-    foreach ($arResult['SECTIONS'] as &$arSection) {
-        $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
-        $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
-
-        ?>
-        <div id="<? echo $this->GetEditAreaId($arSection['ID']); ?>">
-            <div>
-                <h3>
-                    <a href="<? echo $arSection['SECTION_PAGE_URL']; ?>">
-                        <? echo $arSection['NAME']; ?>
-                        <?
-                        if (CModule::IncludeModule("iblock")):
-
-                            $iblock_id = $arParams["IBLOCK_ID"];
-                            $sec = $arSection['ID'];
-                            # show url my elements
-                            $my_elements = CIBlockElement::GetList(
-                                array("ID" => "ASC"),
-                                array("IBLOCK_ID" => $iblock_id, "SECTION_ID" => $sec, "INCLUDE_SUBSECTIONS" => "Y"),
-                                false,
-                                false,
-                                array('ID', 'NAME', 'DETAIL_PAGE_URL', 'PREVIEW_PICTURE')
-                            );
-
-                            while ($ar_fields = $my_elements->GetNext()) {
-                                $img_path = CFile::GetPath($ar_fields["PREVIEW_PICTURE"]);
-                                ?>
-                                <br> <img src="<?echo $img_path?>"><br>
-                                <a href="<?echo urldecode($ar_fields['DETAIL_PAGE_URL'])?>"><?echo $ar_fields['NAME'];?></a><br>
 
 
-                                <?
-                            }
-                        endif;
-                        ?>
-                    </a>
-                </h3>
-            </div>
-        </div>
-        <?
-    }
-    unset($arSection);
-    break;
+                <section class="products">
+                    <div class="container" id="<? echo $this->GetEditAreaId($arSection['ID']); ?>">
+                        <h2><a href="<? echo $arSection['SECTION_PAGE_URL']; ?>"><? echo $arSection['NAME']; ?></a></h2>
+                        <? echo $i; ?>
+                        <div class="slider slick-good-slider">
 
-case 'TILE':
-    foreach ($arResult['SECTIONS'] as &$arSection) {
-        $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
-        $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
 
-        if (false === $arSection['PICTURE'])
-            $arSection['PICTURE'] = array(
-                'SRC' => $arCurView['EMPTY_IMG'],
-                'ALT' => (
-                '' != $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_ALT"]
-                    ? $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_ALT"]
-                    : $arSection["NAME"]
-                ),
-                'TITLE' => (
-                '' != $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_TITLE"]
-                    ? $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_TITLE"]
-                    : $arSection["NAME"]
-                )
-            );
-        ?>
-        <li id="<? echo $this->GetEditAreaId($arSection['ID']); ?>"
-            class="<?= $listColumsClass ?> catalog-section-list-item">
-            <div class="catalog-section-list-tile-img-container">
-                <a
-                        href="<? echo $arSection['SECTION_PAGE_URL']; ?>"
-                        class="catalog-section-list-item-img"
-                        style="background-image:url('<? echo $arSection['PICTURE']['SRC']; ?>');"
-                        title="<? echo $arSection['PICTURE']['TITLE']; ?>"
-                ></a>
-            </div>
-            <? if ('Y' != $arParams['HIDE_SECTION_NAME']) {
-                ?>
-                <div class="catalog-section-list-item-inner">
-                    <h3 class="catalog-section-list-item-title">
-                        <a class="catalog-section-list-item-link" href="<? echo $arSection['SECTION_PAGE_URL']; ?>">
-                            <? echo $arSection['NAME']; ?>
-                        </a>
-                        <? if ($arParams["COUNT_ELEMENTS"] && $arSection['ELEMENT_CNT'] !== null) {
+                            <? if (CModule::IncludeModule("iblock")):
+
+                                $iblock_id = $arParams["IBLOCK_ID"];
+                                $sec = $arSection['ID'];
+                                # show url my elements
+                                $my_elements = CIBlockElement::GetList(
+                                    array("ID" => "ASC"),
+                                    array("IBLOCK_ID" => $iblock_id, "SECTION_ID" => $sec, "INCLUDE_SUBSECTIONS" => "Y"),
+                                    false,
+                                    false,
+                                    array('ID', 'NAME', 'DETAIL_PAGE_URL', 'PREVIEW_PICTURE', 'CATALOG_PRICE_1')
+                                );
+
+                                while ($ar_fields = $my_elements->GetNext()) {
+                                    $img_path = CFile::GetPath($ar_fields["PREVIEW_PICTURE"]);
+                                    ?>
+                                    <div class="slider__item">
+                                        <div class="slider__item-wrp">
+                                            <img src="<? echo $img_path ?>" alt="<? echo $ar_fields['NAME']; ?>">
+                                            <div class="slider__item-content-wrp">
+                                                <h3>
+                                                    <a href="<? echo urldecode($ar_fields['DETAIL_PAGE_URL']) ?>"><? echo $ar_fields['NAME']; ?></a>
+                                                </h3>
+                                                <p><? echo $ar_fields['CATALOG_PRICE_1'] ?>&nbspр</p>
+                                                <p>Артикул</p>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <?
+                                }
+                            endif;
                             ?>
-                            <span class="catalog-section-list-item-counter">(<? echo $arSection['ELEMENT_CNT']; ?>)</span>
-                            <?
-                        }
-                        ?>
-                    </h3>
-                </div>
+                        </div>
+                    </div>
+                </section>
+
                 <?
             }
-            ?>
-        </li>
-        <?
+            unset($arSection);
+            break;
     }
-    unset($arSection);
-    break;
+    ?>
 
-case 'LIST':
-$intCurrentDepth = 1;
-$boolFirst = true;
-foreach ($arResult['SECTIONS'] as &$arSection)
-{
-$this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
-$this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
-
-if ($intCurrentDepth < $arSection['RELATIVE_DEPTH_LEVEL']) {
-    if (0 < $intCurrentDepth)
-        echo "\n", str_repeat("\t", $arSection['RELATIVE_DEPTH_LEVEL']), '<ul>';
-} elseif ($intCurrentDepth == $arSection['RELATIVE_DEPTH_LEVEL']) {
-    if (!$boolFirst)
-        echo '</li>';
-} else {
-    while ($intCurrentDepth > $arSection['RELATIVE_DEPTH_LEVEL']) {
-        echo '</li>', "\n", str_repeat("\t", $intCurrentDepth), '</ul>', "\n", str_repeat("\t", $intCurrentDepth - 1);
-        $intCurrentDepth--;
-    }
-    echo str_repeat("\t", $intCurrentDepth - 1), '</li>';
+    <?
 }
-
-echo(!$boolFirst ? "\n" : ''), str_repeat("\t", $arSection['RELATIVE_DEPTH_LEVEL']);
 ?>
-<li class="catalog-section-list-item" id="<?= $this->GetEditAreaId($arSection['ID']); ?>">
-    <h3 class="catalog-section-list-list-title">
-        <a class="catalog-section-list-list-link"
-           href="<? echo $arSection["SECTION_PAGE_URL"]; ?>"><? echo $arSection["NAME"]; ?><?
-            if ($arParams["COUNT_ELEMENTS"] && $arSection['ELEMENT_CNT'] !== null) {
-                ?> <span>(<? echo $arSection["ELEMENT_CNT"]; ?>)</span><?
-            }
-            ?>
-        </a>
-    </h3>
-    <?
-
-    $intCurrentDepth = $arSection['RELATIVE_DEPTH_LEVEL'];
-    $boolFirst = false;
-    }
-    unset($arSection);
-    while ($intCurrentDepth > 1) {
-        echo '</li>', "\n", str_repeat("\t", $intCurrentDepth), '</ul>', "\n", str_repeat("\t", $intCurrentDepth - 1);
-        $intCurrentDepth--;
-    }
-    if ($intCurrentDepth > 0) {
-        echo '</li>', "\n";
-    }
-    break;
-    }
-    ?>
-
-    <?
-    }
-    ?>
 
 
 
